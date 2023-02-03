878f471aee370d95c5627e9a4453579a65acd1f0
kg <2448723621@qq.com>
2023-02-03 17:29:51 +0800

    first commit

---
+++ src/main/java/com/kg/gitdemo/GitTest.java
@@ -0,0 +1,11 @@
+package com.kg.gitdemo;
+
+/**
+ * @author kkkkkkkkg
+ * @date 2023-02-03 17:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好！");
+    }
+}
